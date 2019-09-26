75e1193aa55d340848b7d224c2d480593f86922e
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:00:39 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan1/biodata/IF0110118025Latihan1BIODATA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan1BIODATA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t: Rizki Restu Illahi");
+        System.out.println("ALAMAT\t\t: Jalan Sekeloa Utara No 214 D");
+        System.out.println("TEMPAT LAHIR\t: Subang");
+        System.out.println("TANGGAL LAHIR\t: 27 April 2000");
+        System.out.println("-----------------");
+        System.out.println("HOBI\t\t: Nonton film");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Es Jeruk");
+    }
+    
+}
